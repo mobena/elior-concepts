0bf1a03d94d162ca40d2744ec55ed56f931846c9
mobena <moamben@gmail.com>
2016-10-24 16:55:30 +0200

    add order type crud

---
+++ app/DoctrineMigrations/Version20161024161200.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161024161200 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE order_type (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_C12F6D3EEA750E8 (label), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE order_type');
+    }
+}
---
+++ src/AppBundle/Controller/OrderTypeController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\OrderType;
+use AppBundle\Form\OrderTypeType;
+
+/**
+ * OrderType controller.
+ *
+ * @Route("/admin/ordertype")
+ */
+class OrderTypeController extends Controller
+{
+    /**
+     * Lists all OrderType entities.
+     *
+     * @Route("/", name="admin_ordertype_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $orderTypes = $em->getRepository('AppBundle:OrderType')->findAll();
+
+        return $this->render('ordertype/index.html.twig', array(
+            'orderTypes' => $orderTypes,
+        ));
+    }
+
+    /**
+     * Creates a new OrderType entity.
+     *
+     * @Route("/new", name="admin_ordertype_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $orderType = new OrderType();
+        $form = $this->createForm('AppBundle\Form\OrderTypeType', $orderType);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($orderType);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_ordertype_show', array('id' => $orderType->getId()));
+        }
+
+        return $this->render('ordertype/new.html.twig', array(
+            'orderType' => $orderType,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a OrderType entity.
+     *
+     * @Route("/{id}", name="admin_ordertype_show")
+     * @Method("GET")
+     */
+    public function showAction(OrderType $orderType)
+    {
+        $deleteForm = $this->createDeleteForm($orderType);
+
+        return $this->render('ordertype/show.html.twig', array(
+            'orderType' => $orderType,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing OrderType entity.
+     *
+     * @Route("/{id}/edit", name="admin_ordertype_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, OrderType $orderType)
+    {
+        $deleteForm = $this->createDeleteForm($orderType);
+        $editForm = $this->createForm('AppBundle\Form\OrderTypeType', $orderType);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($orderType);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_ordertype_edit', array('id' => $orderType->getId()));
+        }
+
+        return $this->render('ordertype/edit.html.twig', array(
+            'orderType' => $orderType,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a OrderType entity.
+     *
+     * @Route("/{id}", name="admin_ordertype_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, OrderType $orderType)
+    {
+        $form = $this->createDeleteForm($orderType);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($orderType);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_ordertype_index');
+    }
+
+    /**
+     * Creates a form to delete a OrderType entity.
+     *
+     * @param OrderType $orderType The OrderType entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(OrderType $orderType)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_ordertype_delete', array('id' => $orderType->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
