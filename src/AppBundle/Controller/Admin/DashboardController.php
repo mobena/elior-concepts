a870eebe1aaff7f912d2fb54040b3cda53623077
mobena <moamben@gmail.com>
2016-10-25 18:07:12 +0200

    add an empty back homepage controller containing dashboard & link correctly from the headers

---
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller used to manage blog contents in the backend.
+ *
+ * @Route("/admin/dashboard")
+ * @Security("has_role('ROLE_ADMIN')")
+ *
+ * @author Mohamed Benabdallah <moamben@gmail.com>
+ */
+class DashboardController extends Controller
+{
+    /**
+     * Dashboard Controller
+     *
+     * @Route("/", name="admin_dashboard_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        //$em = $this->getDoctrine()->getManager();
+
+        return $this->render('dashboard/index.html.twig', array());
+    }
+}
