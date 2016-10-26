67afc467ae757b5971468340256d9d5b6203dadf
mobena <moamben@gmail.com>
2016-10-26 16:19:19 +0200

    add crud for user entity

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Form\Type\DateTimePickerType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('password', PasswordType::class)
+            ->add('roles', CollectionType::class)
+            ->add('createdAt', DateTimePickerType::class, array('disabled' => true))
+            ->add('updatedAt', DateTimePickerType::class, array('required' => false, 'disabled' => true))
+            ->add('deletedAt', DateTimePickerType::class, array('required' => false, 'disabled' => true))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+}
