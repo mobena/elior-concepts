6ab86ae652b9d574eab9bb3825e955551df1ddcd
mobena <moamben@gmail.com>
2016-10-28 17:37:40 +0200

    add product show interface

---
+++ src/AppBundle/Controller/Front/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller\Front;
+
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller used to manage product contents in the frontend.
+ *
+ * @Route("/product")
+ *
+ * @author Mohamed Benabdallah <moamben@gmail.com>
+ */
+class ProductController extends Controller
+{
+    /**
+     * Finds and displays a Product entity.
+     *
+     * @Route("/{id}", name="front_product_show")
+     * @Method("GET")
+     */
+    public function showAction(Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('AppBundle:Product')
+            ->findBy(array('id' => $product, 'productStatus' => 1, 'deletedAt' => null,));
+
+        return $this->render('front/product/show.html.twig', array(
+            'products' => $products,
+        ));
+    }
+}
